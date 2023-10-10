2c6d399e5a9ebeffc54a1670f6b2e894af519eca
DenisCorlade <corladedenis19@gmail.com>
2023-10-10 17:21:16 +0200

    Add SearchBar component UI

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <section className="px-6 md:px-20 py-24 border-2 border-red-500">
-        <div className="flex max-xl :flex-col gap-16">
+        <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
               Find the price history of your desired product:
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,7 +5,16 @@ import React from 'react';
 const SearchBar = () => {
   const handleSubmit = () => {};
   return (
-    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}></form>
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
+    </form>
   );
 };
 
